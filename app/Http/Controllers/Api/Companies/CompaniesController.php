2f99a0d7851124c2536182b0219996eefac834dd
ngtinn59 <tinb2004814@student.ctu.edu.vn>
2024-02-28 16:21:30 +0700

    CV updateeeeeeeeeeeeeeeeeee

---
+++ app/Http/Controllers/Api/Companies/CompaniesController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\Companies;
+
+use App\Models\Company;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CompaniesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Company $company)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Company $company)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Company $company)
+    {
+        //
+    }
+}
